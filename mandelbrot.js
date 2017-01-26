35608f7f75d8da851df6252322b23d109f85d4bc
Lars Christensen <larsch@belunktum.dk>
2017-01-26 16:12:20 +0100

    Autoscale, optimize, etc.

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,10 +1,24 @@
 'use strict';
 let ca = document.getElementById("canvas");
+let cas = document.getElementById("scaled");
+cas.width = ca.width = window.innerWidth - 20;
+cas.height = ca.height = window.innerHeight - 20;
+
 let ct = ca.getContext("2d");
-// ca.width = window.innerWidth - 20;
-// ca.height = window.innerHeight - 20;
+let cts = cas.getContext("2d");
 let w = ca.width;
 let h = ca.height;
+
+let pca = document.getElementById("progress");
+pca.width = 40;
+pca.height = 40;
+pca.style.top = "20px";
+pca.style.left = "20px";
+let pct = pca.getContext("2d");
+
+let ss = 16;
+let sl = Math.floor((h + ss - 1) / ss);
+let h2 = sl * ss;
 let xmin = -2.5;
 let xmax = 1.5;
 let ymin = 1.5;
@@ -14,7 +28,10 @@ let ysize = ymax - ymin;
 let xscale = xsize / w;
 let yscale = ysize / h;
 let y = 0;
-let yGoal = h;
+let yGoal = 0;
+
+cts.fillStyle = "rgb(0,128,0)";
+cts.fillRect(200, 200, w - 200, h - 200);
 
 function hsv2rgb(h, s, v) {
   let hm = h / 60;
@@ -32,21 +49,31 @@ function hsv2rgb(h, s, v) {
 
 let scale = new Array(256);
 for (let i = 0; i < 256; i++) {
-  scale[i] = hsv2rgb((i * 360) / 256, 1.0, 0.5);
+  scale[i] = hsv2rgb(((i * 360 * 4) / 256) % 360, 1.0, 0.5);
 }
 
 let renderStartTime = null;
 
 function anim(t) {
-  let renderEndTime = Date.now() + 50;
+  let renderEndTime = Date.now() + 25;
   let before = y;
   do {
     renderRow(y);
-    y = (y + 1) % h;
+    y = (y + 1) % h2;
   } while (y != yGoal && Date.now() < renderEndTime);
-  let delta = (y + h - before) % h;
-  if (y != yGoal)
+
+  if (y != yGoal) {
+    let progress = ((yGoal + h2 - y) % h2) / h2;
+    pct.clearRect(0,0,pca.width,pca.height);
+    pct.fillStyle = "rgba(255,255,255,0.5)";
+    pct.beginPath();
+    pct.moveTo(20,20);
+    pct.arc(20,20,16,3/2*Math.PI,3/2*Math.PI - 2 * Math.PI * progress, true);
+    pct.fill();
     requestAnimationFrame(anim);
+  } else {
+    pct.clearRect(0,0,pca.width,pca.height);
+  }
   document.getElementById("renderTime").textContent = Date.now() - renderStartTime;
 }
 
@@ -82,18 +109,29 @@ ca.onclick = function(e) {
   let sy = (ysize / ysize1);
   let tx = -dx;
   let ty = -dy;
-  var imageObject = new Image();
-  imageObject.onload = function() {
-    ct.translate(dx, dy);
-    ct.scale(sx, sy);
-    ct.drawImage(imageObject, 0, 0);
-    ct.resetTransform();
-    if (y == yGoal)
-      startRender();
-    else
-      yGoal = y;
-  }
-  imageObject.src = ca.toDataURL();
+
+
+  //  var imageObject = new Image();
+  //  imageObject.onload = function() {
+  //     cts.translate(dx, dy);
+  //     cts.scale(sx, sy);
+  //     cts.drawImage(imageObject, 0, 0);
+  //     cts.resetTransform();
+  //     console.log("background image set");
+  // };
+  // imageObject.src = ca.toDataURL();
+
+  // cts.putImageData(ct.getImageData(0,0,w,h), 0,0);
+  cts.drawImage(ca, 0, 0);
+  cts.translate(dx, dy);
+  cts.scale(sx, sy);
+  cts.drawImage(cas, 0, 0);
+  cts.setTransform(1, 0, 0, 1, 0, 0);
+  ct.clearRect(0,0,w,h);
+  if (y == yGoal)
+    startRender();
+  else
+    yGoal = y;
 
   xmin = xmin1;
   xmax = xmax1;
@@ -103,27 +141,32 @@ ca.onclick = function(e) {
   ysize = ysize1;
   xscale = xsize / w;
   yscale = ysize / h;
-}
+};
 
 let img = new ImageData(w, 1);
 let data = img.data;
 
+function iter(cx, cy) {
+  let zy = cy;
+  let zx = cx;
+  let n = 0;
+  let zx2, zy2;
+  while ((zx2 = zx * zx) + (zy2 = zy * zy) <= 4.0) {
+    zy = 2 * zx * zy + cy;
+    zx = zx2 - zy2 + cx;
+    if (++n == 4096) break;
+  }
+  return n;
+}
+
 function renderRow(y) {
   // map y coordinate to non-linear order
-  // y = (Math.floor(y / 32) * 11) % 15 + (y % 32) * 15;
+  y = (Math.floor(y / sl) * 11) % ss + (y % sl) * ss;
 
   let cy = ymin + yscale * y;
   for (let x = 0; x < w; ++x) {
     let cx = xmin + xscale * x;
-    let zy = cy;
-    let zx = cx;
-    let n = 0;
-    let zx2, zy2;
-    while ((zx2 = zx * zx) + (zy2 = zy * zy) <= 4.0) {
-      zy = 2 * zx * zy + cy;
-      zx = zx2 - zy2 + cx;
-      if (++n == 4096) break;
-    }
+    let n = iter(cx, cy);
     let p = x * 4;
     if (n == 4096) {
       data[p + 0] = data[p + 1] = data[p + 2] = 0;
@@ -138,4 +181,6 @@ function renderRow(y) {
   ct.putImageData(img, 0, y);
 }
 
+ct.fillStyle = "rgb(" + scale[0][0] + "," + scale[0][1] + "," + scale[0][2] + ")";
+ct.fillRect(0,0,w,h);
 startRender();
